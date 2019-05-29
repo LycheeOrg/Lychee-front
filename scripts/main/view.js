c09a0a7fed8c628df48f51fca9ae8ca7583f5cf0
kamil4 <kamil.01482@iskra.name>
2019-05-28 23:04:32 -0500

    Patch up race conditions in search
    Co-Authored-By: kamil4 <kamil.01482@iskra.name>

--- scripts/main/view.js
+++ scripts/main/view.js
@@ -316,7 +316,7 @@ view.album = {
 		},
 
 		justify: function () {
-			if (!album.json.photos || album.json.photos === false) return;
+			if (!album.json || !album.json.photos || album.json.photos === false) return;
 			if (lychee.layout === '1') {
 				let containerWidth = parseFloat($('.justified-layout').width(), 10);
 				if (containerWidth == 0) {
@@ -344,6 +344,12 @@ view.album = {
 				$('.justified-layout').css('height', layoutGeometry.containerHeight + 'px')
 					.css('height', layoutGeometry.containerHeight + 'px');
 				$('.justified-layout > div').each(function (i) {
+					if (!layoutGeometry.boxes[i]) {
+						// Race condition in search.find -- window content
+						// and album.json can get out of sync as search
+						// query is being modified.
+						return false
+					}
 					$(this).css('top', layoutGeometry.boxes[i].top);
 					$(this).css('width', layoutGeometry.boxes[i].width);
 					$(this).css('height', layoutGeometry.boxes[i].height);
@@ -363,6 +369,12 @@ view.album = {
 						parseFloat($('.unjustified-layout').css('margin-right'), 10);
 				}
 				$('.unjustified-layout > div').each(function (i) {
+					if (!album.json.photos[i]) {
+						// Race condition in search.find -- window content
+						// and album.json can get out of sync as search
+						// query is being modified.
+						return false
+					}
 					let ratio = album.json.photos[i].height > 0 ?
 						album.json.photos[i].width / album.json.photos[i].height : 1;
 					if (album.json.photos[i].type && album.json.photos[i].type.indexOf('video') > -1) {
