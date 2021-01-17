c00619ad5202fa8792a534ca2f2f42ee9b9d90a5
kamil4 <kamil.01482@iskra.name>
2021-01-17 17:02:36 -0600

    Don't show button_add for public smart albums
    Also clean up the handling of button_nsfw.

--- scripts/main/header.js
+++ scripts/main/header.js
@@ -297,21 +297,26 @@ header.setMode = function (mode) {
 			}
 
 			if (albumID === "starred" || albumID === "public" || albumID === "recent") {
-				$("#button_info_album, #button_trash_album, #button_visibility_album, #button_move_album").hide();
-				$(".button_add, .header__divider", ".header__toolbar--album").show();
-				tabindex.makeFocusable($(".button_add, .header__divider", ".header__toolbar--album"));
-				tabindex.makeUnfocusable($("#button_info_album, #button_trash_album, #button_visibility_album, #button_move_album"));
+				$("#button_nsfw_album, #button_info_album, #button_trash_album, #button_visibility_album, #button_move_album").hide();
+				if (album.isUploadable()) {
+					$(".button_add, .header__divider", ".header__toolbar--album").show();
+					tabindex.makeFocusable($(".button_add, .header__divider", ".header__toolbar--album"));
+				} else {
+					$(".button_add, .header__divider", ".header__toolbar--album").hide();
+					tabindex.makeUnfocusable($(".button_add, .header__divider", ".header__toolbar--album"));
+				}
+				tabindex.makeUnfocusable($("#button_nsfw_album, #button_info_album, #button_trash_album, #button_visibility_album, #button_move_album"));
 			} else if (albumID === "unsorted") {
-				$("#button_info_album, #button_visibility_album, #button_move_album").hide();
+				$("#button_nsfw_album, #button_info_album, #button_visibility_album, #button_move_album").hide();
 				$("#button_trash_album, .button_add, .header__divider", ".header__toolbar--album").show();
 				tabindex.makeFocusable($("#button_trash_album, .button_add, .header__divider", ".header__toolbar--album"));
-				tabindex.makeUnfocusable($("#button_info_album, #button_visibility_album, #button_move_album"));
+				tabindex.makeUnfocusable($("#button_nsfw_album, #button_info_album, #button_visibility_album, #button_move_album"));
 			} else if (album.isTagAlbum()) {
 				$("#button_info_album").show();
-				$("#button_move_album").hide();
+				$("#button_nsfw_album, #button_move_album").hide();
 				$(".button_add, .header__divider", ".header__toolbar--album").hide();
 				tabindex.makeFocusable($("#button_info_album"));
-				tabindex.makeUnfocusable($("#button_move_album"));
+				tabindex.makeUnfocusable($("#button_nsfw_album, #button_move_album"));
 				tabindex.makeUnfocusable($(".button_add, .header__divider", ".header__toolbar--album"));
 				if (album.isUploadable()) {
 					$("#button_visibility_album, #button_trash_album").show();
@@ -330,7 +335,7 @@ header.setMode = function (mode) {
 					).show();
 					tabindex.makeFocusable(
 						$(
-							"#button_trash_album, #button_move_album, #button_visibility_album, .button_add, .header__divider",
+							"#button_nsfw_album, #button_trash_album, #button_move_album, #button_visibility_album, .button_add, .header__divider",
 							".header__toolbar--album"
 						)
 					);
@@ -341,7 +346,7 @@ header.setMode = function (mode) {
 					).hide();
 					tabindex.makeUnfocusable(
 						$(
-							"#button_trash_album, #button_move_album, #button_visibility_album, .button_add, .header__divider",
+							"#button_nsfw_album, #button_trash_album, #button_move_album, #button_visibility_album, .button_add, .header__divider",
 							".header__toolbar--album"
 						)
 					);
