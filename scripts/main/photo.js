cc5d4311e50d5fecdc7c68cd45950b13629588f8
kamil4 <kamil.01482@iskra.name>
2021-01-19 00:14:11 -0600

    Reload albums on tag edits if tag albums present

--- scripts/main/photo.js
+++ scripts/main/photo.js
@@ -820,7 +820,17 @@ photo.setTags = function (photoIDs, tags) {
 	};
 
 	api.post("Photo::setTags", params, function (data) {
-		if (data !== true) lychee.error(null, params, data);
+		if (data !== true) {
+			lychee.error(null, params, data);
+		} else if (albums.json && albums.json.smartalbums) {
+			$.each(Object.entries(albums.json.smartalbums), function() {
+				if (this.length == 2 && this[1]["tag_album"] === "1") {
+					// If we have any tag albums, force a refresh.
+					albums.refresh();
+					return false;
+				}
+			});
+		}
 	});
 };
 
