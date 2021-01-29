96fbce4ca8a61066f23c5cdde8d61a57c857e590
kamil4 <kamil.01482@iskra.name>
2021-01-28 23:20:01 -0600

    Minor tweaks

--- scripts/main/lychee_locale.js
+++ scripts/main/lychee_locale.js
@@ -51,9 +51,9 @@ lychee.locale = {
 	FULLSCREEN_EXIT: "Exit Fullscreen",
 
 	SHARING_ALBUM_USERS: "Share this album with users",
-	SHARING_ALBUM_USERS_LONG_MESSAGE: "Select the users to share this album with them",
-	WAIT_FETCH_DATA: "Please wait while we get the data.",
-	SHARING_ALBUM_USERS_NO_USERS: "There's no user to share the album with.",
+	SHARING_ALBUM_USERS_LONG_MESSAGE: "Select the users to share this album with",
+	WAIT_FETCH_DATA: "Please wait while we get the data...",
+	SHARING_ALBUM_USERS_NO_USERS: "There are no users to share the album with",
 
 	DELETE_ALBUM_QUESTION: "Delete Album and Photos",
 	KEEP_ALBUM: "Keep Album",
