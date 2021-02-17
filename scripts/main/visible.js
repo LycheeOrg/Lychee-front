edc283776ce003fa63b6b05ae44e512bcaad119a
kamil4 <kamil.01482@iskra.name>
2021-02-16 23:53:02 -0600

    Fix visible.search()

--- scripts/main/visible.js
+++ scripts/main/visible.js
@@ -26,7 +26,7 @@ visible.mapview = function () {
 };
 
 visible.search = function () {
-	if (search.hash != null) return true;
+	if (visible.albums() && album.json && album.isSearchID(album.json.id)) return true;
 	return false;
 };
 
