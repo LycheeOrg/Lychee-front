62e2ac328c5ac6f003e3756fdac19690c112c9f6
Matthias Nagel <matthias.h.nagel@posteo.de>
2022-05-01 11:58:41 +0200

    Reverted @kamil4's suggestion and moved it to the correct condition

--- scripts/main/album.js
+++ scripts/main/album.js
@@ -35,7 +35,7 @@ album.isModelID = function (id) {
  * @returns {?string}
  */
 album.getParentID = function () {
-	if (album.json === null || album.isSmartID(album.json.id) || /* album.isSearchID(album.json.id) || */ !album.json.parent_id) {
+	if (album.json === null || album.isSmartID(album.json.id) || album.isSearchID(album.json.id) || !album.json.parent_id) {
 		return null;
 	}
 	return album.json.parent_id;
@@ -50,7 +50,7 @@ album.getID = function () {
 
 	// this is a Lambda
 	let isID = (_id) => {
-		return album.isSmartID(_id) || album.isSearchID(_id) || album.isModelID(_id);
+		return album.isSmartID(_id) || /*album.isSearchID(_id) || */ album.isModelID(_id);
 	};
 
 	if (photo.json) id = photo.json.album_id;
