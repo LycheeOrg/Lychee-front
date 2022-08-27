bcefc0c49fee5dd25f3a8420f5b7bfb27a8ff429
Matthias Nagel <matthias.h.nagel@posteo.de>
2022-08-27 16:21:38 +0200

    Adopted jsDoc for basic modal

--- scripts/3rd-party/basicModal.js
+++ scripts/3rd-party/basicModal.js
@@ -7,44 +7,105 @@
  */
 
 /**
- * Returns an associative object containing the values from all `input` and
- * `select` elements.
+ * @typedef ModalDialogConfiguration
+ * @property {string}                 [body='']       HTML snippet to be inserted into the content
+ *                                                    area of the dialog
+ * @property {string[]}               [classList=[]]  CSS class to be applied to the content area
+ *                                                    of the dialog
+ * @property {boolean}                [closable=true] indicates whether the dialog can be closed
+ *                                                    via {@link close}
+ * @property {ModalDialogButtonsData} buttons         configuration data for the main action and
+ *                                                    cancel button
+ * @property {ModalDialogReadyCB}     [readyCB=null]  callback to be called after the dialog
+ *                                                    has become visible and ready for user input
+ */
+
+/**
+ * @callback ModalDialogReadyCB
+ * @param {ModelDialogFormElements} htmlElements a dictionary that maps names to form elements
+ * @param {HTMLDivElement}          dialog       the DIV element that represents the dialog
+ * @returns {void}
+ */
+
+/**
+ * @typedef ModelDialogFormElements
  *
- * The properties of the returned object correspond to the `name` attribute
- * of the `input` and `select` elements.
+ * A dictionary of names of form elements to those form elements.
+ *
+ * @type {Object.<string, (HTMLInputElement|HTMLTextAreaElement|HTMLSelectElement)>}
+ */
+
+/**
+ * @typedef ModalDialogButtonsData
+ * @property {ModalDialogButtonData} [action] configuration data for the main action button
+ * @property {ModalDialogButtonData} [cancel] configuration data for the cancel button
+ */
+
+/**
+ * @typedef ModalDialogButtonData
+ * @property {string}                  [title]         the caption of the button
+ * @property {string[]}                [classList=[]]  CSS class to be applied to the button
+ * @property {Object.<string, string>} [attributes={}] a dictionary of arbitrary HTML attributes and their values
+ * @property {ModalDialogButtonCB}     fn              callback to be called upon an "on-click" event
+ */
+
+/**
+ * @callback ModalDialogButtonCB
+ * @param {ModalDialogResult} values an associative object with the values of
+ *                                   all HTML input elements; see {@link getValues}
+ * @returns {void}
+ */
+
+/**
+ * A dictionary that maps name of form elements to their value.
+ *
+ * @typedef ModalDialogResult
+ * @type {Object.<string,(number|string|boolean|FileList)>}
+ */
+
+/**
+ * @typedef ModalDialogException
+ * @property {string} name
+ * @property {string} message
+ */
+
+/**
+ * Returns an associative object containing the values from all HTML form
+ * elements.
  *
  * @function getValues
  * @memberOf basicModal
- * @returns {Object}
+ * @returns {ModalDialogResult}
  */
 
 /**
  * Constructs and shows a modal dialog.
  *
- * After the dialog has become ready, the callback `data.callback` is
+ * After the dialog has become ready, the callback `confData.readyCB` is
  * invoked.
  *
  * @function show
  * @memberOf basicModal
- * @param {ModalDialogData} data configuration data for the dialog
- * @returns {boolean} `true` if the dialog became visible
+ * @param {ModalDialogConfiguration} confData configuration data for the dialog
+ * @returns {void}
+ * @throws {ModalDialogException}
  */
 
 /**
  * Removes (potentially) old error indicators and highlights the indicated
  * input element.
  *
- * @function error
+ * @function focusError
  * @memberOf basicModal
- * @param {string} [nameAttribute] the name of the HTML input element which
- *                                 caused the error and shall be highlighted
+ * @param {string} [name] the name of the HTML input element which
+ *                        caused the error and shall be highlighted
  * @returns {void}
  */
 
 /**
  * Determines whether a modal dialog is visible or not.
  *
- * @function visible
+ * @function isVisible
  * @memberOf basicModal
  * @returns {boolean}
  */
@@ -74,11 +135,12 @@
  */
 
 /**
- * Removes any (potential) error indicator from the input elements.
+ * Reactivates buttons and removes any (potential) error indicator from the
+ * input elements.
  *
  * @function reset
  * @memberOf basicModal
- * @returns {boolean} always `true`
+ * @returns {void}
  */
 
 /**
@@ -87,48 +149,5 @@
  * @function close
  * @memberOf basicModal
  * @param {boolean} [force=false]
- * @returns {boolean} `true`, if the dialog has been visible before and has
- *                     been closed;
- *                    `false`, if no dialog has been visible which could be
- *                    closed
- */
-
-/**
- * @typedef ModalDialogData
- * @property {string}                 [body='']       HTML snippet to be inserted into the content
- *                                                    area of the dialog
- * @property {string}                 [class='']      CSS class to be applied to the content area
- *                                                    of the dialog
- * @property {boolean}                [closable=true] indicates whether the dialog can be closed
- *                                                    via {@link basicModal.close}
- * @property {ModalDialogButtonsData} buttons         configuration data for the main action and
- *                                                    cancel button
- * @property {ModalDialogReadyCB}     [callback=null] callback to be called after the dialog
- *                                                    has become visible and ready for user input
- */
-
-/**
- * @callback ModalDialogReadyCB
- * @param {ModalDialogData} data the configuration data which has been used to construct the dialog
- * @returns {void}
- */
-
-/**
- * @typedef ModalDialogButtonsData
- * @property {ModalDialogButtonData} [action] configuration data for the main action button
- * @property {ModalDialogButtonData} [cancel] configuration data for the cancel button
- */
-
-/**
- * @typedef ModalDialogButtonData
- * @property {string}              [title] the caption of the button
- * @property {string}              [class] CSS class to be applied to the button
- * @property {ModalDialogButtonCB} fn      callback to be called upon an "on-click" event
- */
-
-/**
- * @callback ModalDialogButtonCB
- * @param {Object} [values] an associative object with the values of all HTML
- *                          input elements; see {@link basicModal.getValues}
  * @returns {void}
  */
