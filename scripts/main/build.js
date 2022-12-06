e16eecf3fb12ff67c1ae1e6e988d9c225b838452
ildyria <beviguier@gmail.com>
2022-12-06 23:41:48 +0100

    Format

--- scripts/main/build.js
+++ scripts/main/build.js
@@ -177,7 +177,7 @@ build.album = function (data, disabled = false) {
 					<a class='badge ${data.id === SmartAlbumID.RECENT ? "badge--visible badge--list" : ""}'>${build.iconic("clock")}</a>
 					<a class='badge ${data.id === SmartAlbumID.ON_THIS_DAY ? "badge--tag badge--list" : ""}'>${build.iconic("calendar")}</a>
 					<a class='badge ${data.id === SmartAlbumID.PUBLIC || (data.policy && data.policy.is_public) ? "badge--visible" : ""} ${
-						data.policy && data.policy.is_link_required ? "badge--hidden" : "badge--not--hidden"
+			data.policy && data.policy.is_link_required ? "badge--hidden" : "badge--not--hidden"
 		} icn-share'>${build.iconic("eye")}</a>
 					<a class='badge ${data.id === SmartAlbumID.UNSORTED ? "badge--visible" : ""}'>${build.iconic("list")}</a>
 					<a class='badge ${data.policy && data.policy.is_password_required ? "badge--visible" : ""}'>${build.iconic("lock-unlocked")}</a>
