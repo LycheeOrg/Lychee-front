cce0dd53a1ea890041fe157aac85729a27a0a7ae
qwerty287 <ndev@web.de>
2023-02-05 10:21:04 +0100

    Update dependencies

--- scripts/main/notifications.js
+++ scripts/main/notifications.js
@@ -9,7 +9,8 @@ const notifications = {
  */
 notifications.update = function (params) {
 	if (params.email && params.email.length > 1) {
-		const regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		const regexp =
+			/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 		if (!regexp.test(String(params.email).toLowerCase())) {
 			loadingBar.show("error", lychee.locale["ERROR_INVALID_EMAIL"]);
