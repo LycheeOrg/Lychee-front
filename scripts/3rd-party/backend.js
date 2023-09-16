ab05bcd10c739a59363958fb742ea1a62d428bf3
ildyria <beviguier@gmail.com>
2023-09-16 10:07:56 +0200

    WRTC

--- scripts/3rd-party/backend.js
+++ scripts/3rd-party/backend.js
@@ -316,7 +316,7 @@ const SmartAlbumID = Object.freeze({
  * @property {string}   landing_page_enable      - actually a boolean
  * @property {string}   lang
  * @property {string[]} lang_available
- * @property {string}   layout                   - actually a number: `square`, `justified` or `unjustified`
+ * @property {string}   layout                   - `square`, `justified` or `unjustified`
  * @property {string}   [location]
  * @property {string}   location_decoding        - actually a boolean
  * @property {string}   location_show            - actually a boolean
